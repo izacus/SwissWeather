711133ee3b5e2b8f0a4266bca117c7f236fba826
Jernej Virag <jernej@virag.si>
2022-08-21 13:30:07 +0200

    Rework of forecast using mobile API

--- meteo/meteo.py
+++ meteo/meteo.py
@@ -5,11 +5,36 @@ import requests
 import logging
 import json
 from datetime import datetime, timezone
+from typing import NewType
+
 
 logger = logging.getLogger(__name__)
 
 STATION_URL = "https://data.geo.admin.ch/ch.meteoschweiz.messnetz-automatisch/ch.meteoschweiz.messnetz-automatisch_en.csv"
 CURRENT_CONDITION_URL= 'https://data.geo.admin.ch/ch.meteoschweiz.messwerte-aktuell/VQHA80.csv'
+
+FORECAST_URL= "https://app-prod-ws.meteoswiss-app.ch/v1/plzDetail?plz={}00"
+FORECAST_USER_AGENT = "android-31 ch.admin.meteoswiss-2160000"
+
+CONDITION_CLASSES = {
+    "clear-night": [101],
+    "cloudy": [5,35,105,135],
+    "fog": [27,28,127,128],
+    "hail": [],
+    "lightning": [12,112],
+    "lightning-rainy": [13,23,24,25,32,113,123,124,125,132],
+    "partlycloudy": [2,3,4,102,103,104],
+    "pouring": [20,120],
+    "rainy": [6,9,14,17,29,33,106,109,114,117,129,133],
+    "snowy": [8,11,16,19,22,30,34,108,111,116,119,122,130,134],
+    "snowy-rainy": [7,10,15,18,21,31,107,110,115,118,121,131],
+    "sunny": [1,26,126],
+    "windy": [],
+    "windy-variant": [],
+    "exceptional": [],
+}
+
+ICON_TO_CONDITION_MAP =  {i: k for k, v in CONDITION_CLASSES.items() for i in v}
 
 """
 Returns float or None
@@ -23,6 +48,17 @@ def to_float(string: str) -> float:
     except ValueError:
         return None
 
+def to_int(string: str) -> float:
+    if string is None:
+        return None
+
+    try:
+        return int(string)
+    except ValueError:
+        return None
+
+FloatValue = NewType('FloatValue', tuple[float, str])
+
 @dataclass
 class StationInfo(object):
     name: str
@@ -32,7 +68,6 @@ class StationInfo(object):
     lat: float
     lng: float
     canton: str
-    postCode: int = 0
 
     def __str__(self) -> str:
         return f"Station {self.abbreviation} - [Name: {self.name}, Lat: {self.lat}, Lng: {self.lng}, Canton: {self.canton}]"
@@ -40,20 +75,48 @@ class StationInfo(object):
     def __hash__(self) -> int:
         return hash(self.name, self.abbreviation)
 
+@dataclass
+class CurrentWeather(object):
+    station: StationInfo
+    date: datetime
+    airTemperature: FloatValue
+    precipitation: FloatValue
+    sunshine: FloatValue
+    globalRadiation: FloatValue
+    relativeHumidity: FloatValue
+    dewPoint: FloatValue
+    windDirection: FloatValue
+    windSpeed: FloatValue
+    gustPeak1s: FloatValue
+    pressureStationLevel: FloatValue
+    pressureSeaLevel: FloatValue
+    pressureSeaLevelAtStandardAtmosphere: FloatValue
+
+@dataclass
+class CurrentState(object):
+    currentTemperature: FloatValue
+    currentIcon: int
+    currentCondition: str    
+
+@dataclass
+class Forecast(object):
+    timestamp: datetime
+    icon: int
+    condition: str
+    temperatureMax: FloatValue
+    temperatureMin: FloatValue
+    precipitation: FloatValue
+
+@dataclass
+class WeatherForecast(object):
+    current: CurrentState
+    dailyForecast: list[Forecast]
+
 class MeteoClient(object):
     def __init__(self):
         logger.debug("Initializing client.")
 
-    def get_data_for_station(self, station: str):
-        current_state = self.get_current_weather_for_station(station)
-        extended_station = self._get_extended_station(station)
-        if extended_station is not None and extended_station.postCode > 0:
-            current_state["station"] = extended_station
-            forecast = self.get_forecast(extended_station.postCode)
-            current_state["forecast"] = forecast
-        return current_state
-
-    def get_current_weather_for_all_stations(self):
+    def get_current_weather_for_all_stations(self) -> list[CurrentWeather]:
         logger.debug("Retrieving current weather for all stations ...")
         data = self._get_csv_dictionary_for_url(CURRENT_CONDITION_URL)
         weather = []
@@ -61,7 +124,7 @@ class MeteoClient(object):
             weather.append(self._get_current_data_for_row(row))
         return weather
 
-    def get_current_weather_for_station(self, station: str):
+    def get_current_weather_for_station(self, station: str) -> CurrentWeather:
         logger.debug("Retrieving current weather...")
         data = self._get_current_weather_line_for_station(station)
         if data is None:
@@ -93,57 +156,28 @@ class MeteoClient(object):
             )      
         return stations
 
-    def _get_extended_station(self, abbreviation: str) -> StationInfo:
-        stations = self.get_all_stations()
-        station = stations.get(abbreviation)
-        if station is None:
-            return None
-
-        # Resolve post code
-        if station.postCode == 0:
-            code = self._get_post_code_for_station(station.lat, station.lng)
-            if code is not None:
-                logger.info(f"Resolved {abbreviation} to {code}.")
-                station.postCode = int(code)
-        return station
-
-    def _get_post_code_for_station(self, lat: float, lng: float) -> int:
-        s = requests.Session()
-        lat = str(lat)
-        lng = str(lng)
-        s.headers.update({"Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-                "Accept-Encoding":"gzip, deflate, sdch",'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/1337 Safari/537.36'})
-        geoData = s.get("https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat="+lat+"&lon="+lng+"&zoom=18").text
-        logger.debug("Got data from opensteetmap: %s"%(geoData))
-        geoData = json.loads(geoData)
-        try:
-            return geoData["address"]["postcode"]
-        except:
-            logger.warning("Unable to get post code for location lat : %s lon : %s"%(lat,lng))
-            return None
-
     def _get_current_data_for_row(self, csv_row):
         timestamp = None
         timestamp_raw = csv_row.get('Date', None)
         if timestamp_raw is not None:
             timestamp = datetime.strptime(timestamp_raw, '%Y%m%d%H%M').replace(tzinfo=timezone.utc)
         
-        station_data = {
-            "station": self.get_all_stations().get(csv_row.get('Station/Location')),
-            "date": timestamp,
-            "air_temperature": (to_float(csv_row.get('tre200s0', None)), "°C") ,
-            "precipitation_10min": (to_float(csv_row.get('rre150z0', None)), "mm"),
-            "sunshine_10min": (to_float(csv_row.get('sre000z0', None)), "min"),
-            "global_radiation_10min": (to_float(csv_row.get('gre000z0', None)), "W/m²"),
-            "relative_humidity": (to_float(csv_row.get('ure200s0', None)), '%'),
-            "dew_point": (to_float(csv_row.get('tde200s0', None)), '°C'),
-            "wind_direction_10min": (to_float(csv_row.get('dkl010z0', None)), '°'),
-            "wind_speed_10min": (to_float(csv_row.get('fu3010z0', None)), 'km/h'),
-            "gust_peak_1s": (to_float(csv_row.get('fu3010z1', None)), 'km/h'),
-            "pressure_station_level": (to_float(csv_row.get('prestas0', None)), 'hPa'),
-            "pressure_sea_level": (to_float(csv_row.get('prestas0', None)), 'hPa'),
-            "pressure_sea_level_at_std_atmosphere": (to_float(csv_row.get('pp0qnhs0', None)), 'hPa'),
-        }
+        station_data = CurrentWeather(
+            self.get_all_stations().get(csv_row.get('Station/Location')),
+            timestamp,
+            (to_float(csv_row.get('tre200s0', None)), "°C") ,
+            (to_float(csv_row.get('rre150z0', None)), "mm"),
+            (to_float(csv_row.get('sre000z0', None)), "min"),
+            (to_float(csv_row.get('gre000z0', None)), "W/m²"),
+            (to_float(csv_row.get('ure200s0', None)), '%'),
+            (to_float(csv_row.get('tde200s0', None)), '°C'),
+            (to_float(csv_row.get('dkl010z0', None)), '°'),
+            (to_float(csv_row.get('fu3010z0', None)), 'km/h'),
+            (to_float(csv_row.get('fu3010z1', None)), 'km/h'),
+            (to_float(csv_row.get('prestas0', None)), 'hPa'),
+            (to_float(csv_row.get('prestas0', None)), 'hPa'),
+            (to_float(csv_row.get('pp0qnhs0', None)), 'hPa'),
+        )
 
         return station_data
 
@@ -157,6 +191,53 @@ class MeteoClient(object):
                 lines = (line.decode(encoding) for line in r.iter_lines())
                 for row in csv.DictReader(lines, delimiter=';'):
                     yield row
+        except requests.exceptions.RequestException as e:
+            logger.error("Connection failure.", exc_info=1)
+            return None            
+
+    ## Forecast
+    def get_forecast(self, postCode, language="en") -> WeatherForecast:
+        forecastJson = self._get_forecast_json(postCode, langugage=language)
+        currentState = self._get_current_state(forecastJson)
+        dailyForecast = self._get_daily_forecast(forecastJson)
+        return WeatherForecast(currentState, dailyForecast)    
+
+    def _get_current_state(self, forecastJson) -> CurrentState:
+        if "currentWeather" not in forecastJson:
+            return None
+
+        currentIcon = to_int(forecastJson.get('currentWeather', {}).get('icon', None))
+        currentCondition = None
+        if currentIcon is not None:
+            currentCondition = ICON_TO_CONDITION_MAP.get(currentIcon, None)
+        return CurrentState(
+            (to_float(forecastJson.get('currentWeather', {}).get('temperature')), "°C"),
+            currentIcon, currentCondition)        
+        
+    def _get_daily_forecast(self, forecastJson) -> list[Forecast]:
+        forecast = []
+        if "forecast" not in forecastJson:
+            return forecast
+
+        for dailyJson in forecastJson["forecast"]:
+            timestamp = None
+            if "dayDate" in dailyJson:
+                timestamp = datetime.strptime(dailyJson["dayDate"], '%Y-%m-%d')
+            icon = to_int(dailyJson.get('iconDay', None))
+            condition = ICON_TO_CONDITION_MAP.get(icon, None)
+            temperatureMax = (to_float(dailyJson.get('temperatureMax', None)), "°C")
+            temperatureMin = (to_float(dailyJson.get('temperatureMin', None)), "°C")
+            precipitation = (to_float(dailyJson.get('precipitation', None)), "mm")
+            forecast.append(Forecast(timestamp, icon, condition, temperatureMax, temperatureMin, precipitation))
+        return forecast
+
+    def _get_forecast_json(self, postCode, langugage="en"):
+        try:
+            url = FORECAST_URL.format(postCode)
+            return requests.get(url, headers = 
+                { "User-Agent": FORECAST_USER_AGENT, 
+                  "Accept-Language": langugage, 
+                  "Accept": "application/json" }).json()
         except requests.exceptions.RequestException as e:
             logger.error("Connection failure.", exc_info=1)
             return None            
